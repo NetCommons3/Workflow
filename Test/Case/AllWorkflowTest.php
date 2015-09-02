3c6132d4e749daaa775dc49d308374064f20aabe
s-nakajima <nakajimashouhei@gmail.com>
2015-09-02 09:52:20 +0900

    Initial commit

---
+++ Test/Case/AllWorkflowTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Workflow All Test Case
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+/**
+ * Workflow All Test Case
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Workflow\Test\Case
+ * @codeCoverageIgnore
+ */
+class AllWorkflowTest extends CakeTestSuite {
+
+/**
+ * All test suite
+ *
+ * @return CakeTestSuite
+ */
+	public static function suite() {
+		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
+		$suite = new CakeTestSuite(sprintf('All %s Plugin tests', $plugin));
+
+		$directory = CakePlugin::path($plugin) . 'Test' . DS . 'Case';
+		$Folder = new Folder($directory);
+		$exceptions = array();
+		$files = $Folder->tree(null, $exceptions, 'files');
+		foreach ($files as $file) {
+			if (substr($file, -4) === '.php') {
+				$suite->addTestFile($file);
+			}
+		}
+
+		return $suite;
+	}
+}
