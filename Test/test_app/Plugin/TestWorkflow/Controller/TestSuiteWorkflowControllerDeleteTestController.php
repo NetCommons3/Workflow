0fc3d6360d8608b32c8a9cab425069140520a6dd
s-nakajima <nakajimashouhei@gmail.com>
2016-02-16 08:46:11 +0900

    WorkflowControllerDeleteTest::testDeleteGet()のテスト

---
+++ Test/test_app/Plugin/TestWorkflow/Controller/TestSuiteWorkflowControllerDeleteTestController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * WorkflowControllerDeleteTestテスト用Controller
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('AppController', 'Controller');
+
+/**
+ * WorkflowControllerDeleteTestテスト用Controller
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Workflow\Test\test_app\Plugin\TestWorkflow\Controller
+ */
+class TestSuiteWorkflowControllerDeleteTestController extends AppController {
+
+/**
+ * delete
+ *
+ * @return void
+ */
+	public function delete() {
+		$this->autoRender = true;
+	}
+
+}
