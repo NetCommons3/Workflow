43c983a0be235b7112e30190e4cd7936ea72d712
s-nakajima <nakajimashouhei@gmail.com>
2016-02-16 10:05:19 +0900

    WorkflowControllerDeleteTest::testDeletePost()のテスト

--- Test/Case/TestSuite/WorkflowControllerDeleteTest/TestDeletePostTest.php
+++ Test/Case/TestSuite/WorkflowControllerDeleteTest/TestDeletePostTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * WorkflowControllerAddTest::testAddPost()のテスト
+ * WorkflowControllerDeleteTest::testDeletePost()のテスト
  *
  * @author Noriko Arai <arai@nii.ac.jp>
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
@@ -12,19 +12,21 @@
 App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
 
 /**
- * WorkflowControllerAddTest::testAddPost()のテスト
+ * WorkflowControllerDeleteTest::testDeletePost()のテスト
  *
  * @author Shohei Nakajima <nakajimashouhei@gmail.com>
- * @package NetCommons\Workflow\Test\Case\TestSuite\WorkflowControllerAddTest
+ * @package NetCommons\Workflow\Test\Case\TestSuite\WorkflowControllerDeleteTest
  */
-class TestSuiteWorkflowControllerAddTestTestAddPostTest extends NetCommonsControllerTestCase {
+class TestSuiteWorkflowControllerDeleteTestTestDeletePostTest extends NetCommonsControllerTestCase {
 
 /**
  * Fixtures
  *
  * @var array
  */
-	public $fixtures = array();
+	public $fixtures = array(
+		'plugin.workflow.workflow_comment',
+	);
 
 /**
  * Plugin name
@@ -43,32 +45,31 @@ class TestSuiteWorkflowControllerAddTestTestAddPostTest extends NetCommonsContro
 
 		//テストプラグインのロード
 		NetCommonsCakeTestCase::loadTestPlugin($this, 'Workflow', 'TestWorkflow');
-		App::uses('TestSuiteWorkflowControllerAddTest', 'TestWorkflow.TestSuite');
-		$this->TestSuite = new TestSuiteWorkflowControllerAddTest();
+		App::uses('TestSuiteWorkflowControllerDeleteTest', 'TestWorkflow.TestSuite');
+		$this->TestSuite = new TestSuiteWorkflowControllerDeleteTest();
 	}
 
 /**
- * testAddPost()のテスト
+ * testDeletePost()のテスト
  *
  * @return void
  */
-	public function testTestAddPost() {
+	public function testTestDeletePost() {
 		//データ生成
 		$data = array();
 		$role = Role::ROOM_ROLE_KEY_ROOM_ADMINISTRATOR;
-		$urlOptions = array('action' => 'add_post');
+		$urlOptions = array('action' => 'delete_post');
 		$exception = null;
 		$return = 'view';
 
 		//テスト実施
-		$result = $this->TestSuite->testAddPost($data, $role, $urlOptions, $exception, $return);
+		$result = $this->TestSuite->testDeletePost($data, $role, $urlOptions, $exception, $return);
 
 		//チェック
-		$this->assertEquals('add_post', $result->controller->view);
+		$this->assertEquals('delete_post', $result->controller->view);
 
 		$header = $result->controller->response->header();
-		$assertUrl = Inflector::underscore('TestWorkflow') . '/' . Inflector::underscore('TestSuiteWorkflowControllerAddTest') . '/index';
+		$assertUrl = Inflector::underscore('TestWorkflow') . '/' . Inflector::underscore('TestSuiteWorkflowControllerDeleteTest') . '/index';
 		$this->assertContains($assertUrl, $header['Location']);
 	}
-
 }
