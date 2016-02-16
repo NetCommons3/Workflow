18fb1ca2e4dba569691338f64224ac86b8a5627d
s-nakajima <nakajimashouhei@gmail.com>
2016-02-16 16:06:57 +0900

    WorkflowControllerIndexTest::testIndex()のテスト

---
+++ Test/Case/TestSuite/WorkflowControllerIndexTest/TestIndexTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * WorkflowControllerIndexTest::testIndex()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsControllerTestCase', 'NetCommons.TestSuite');
+
+/**
+ * WorkflowControllerIndexTest::testIndex()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Workflow\Test\Case\TestSuite\WorkflowControllerIndexTest
+ */
+class TestSuiteWorkflowControllerIndexTestTestIndexTest extends NetCommonsControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'workflow';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//テストプラグインのロード
+		NetCommonsCakeTestCase::loadTestPlugin($this, 'Workflow', 'TestWorkflow');
+		App::uses('TestSuiteWorkflowControllerIndexTest', 'TestWorkflow.TestSuite');
+		$this->TestSuite = new TestSuiteWorkflowControllerIndexTest();
+	}
+
+/**
+ * testIndex()のテスト
+ *
+ * @return void
+ */
+	public function testTestIndex() {
+		//データ生成
+		$urlOptions = array();
+		$assert = array('method' => 'assertNotEmpty');
+		$exception = null;
+		$return = 'view';
+
+		//テスト実施
+		$result = $this->TestSuite->testIndex($urlOptions, $assert, $exception, $return);
+
+		//チェック
+		$this->assertNull($result->controller->viewVars['username']);
+
+		$pattern = '/' . preg_quote('TestSuite/WorkflowControllerIndexTest/index.ctp', '/') . '/';
+		$this->assertRegExp($pattern, $result->view);
+	}
+
+}
